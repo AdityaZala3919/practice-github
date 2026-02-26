9e2090f8656719bd900b81138dbc3a162321c31c
adityasinh-zala <adityasinhz@webelight.co.in>
2026-02-26 16:00:44 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI(title="Practice Project for Git")
+
+@app.get("/")
+def root():
+    return RedirectResponse("/docs")
