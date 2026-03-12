437575a783f5ba13d5cecad06bdeabf78f9dd6d7
ayushkotadiya5499 <142094873+ayushkotadiya5499@users.noreply.github.com>
2026-03-12 20:10:46 +0530

    done

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-app = FastAPI(title="Practice Project for Git")
+app = FastAPI(title="Practice Project for Git hey ayush")
 
 @app.get("/")
 def root():
